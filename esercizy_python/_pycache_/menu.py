a5f47585d15a9ed6e69af732912084ced0244f2e
Mandeep <mandeep160417@gmail.com>
2026-04-22 11:38:09 +0200

    menu gioco

--- esercizy_python/_pycache_/menu.py
+++ esercizy_python/_pycache_/menu.py
@@ -1,5 +1,31 @@
-import Dadi
-import FizzBuzz
+import time
 
-def avvia_Menu():
-    while True:
+print("=======MENU=========")
+print("Seleziona il gioco che vuoi giocare: ")
+
+scelta = ""
+while scelta != 3:
+    print("1. Gioco dei dadi")
+    print("2. Gioco FizzBuzz")
+    print("3. Esci")
+
+    scelta = int(input("Scegli il numero del gioco che vuoi giocare: \n"))
+
+    if scelta == 1:
+        print("Hai scelto il gioco dei dadi.")
+        time.sleep(1)
+        print("Caricamento in corso...")
+        time.sleep(3)
+        import Dadi
+    elif scelta == 2:
+        print("Hai scelto il gioco FizzBuzz.")
+        time.sleep(1)
+        print("Caricamento in corso...")
+        time.sleep(3)
+        import FizzBuzz
+    elif scelta == 3:
+        print("Grazie, torna a giocare quando vuoi.")
+        break
+    else:
+        print("Digita un numero corretto")
+print("Programma terminato")
